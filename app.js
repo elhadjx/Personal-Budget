7c9dd670ff5e1fb0931cd78c1fb24816ea1dedc9
Hadj H <36198012+elhadjx@users.noreply.github.com>
2024-08-19 14:21:17 +0100

    retrieve all envelopes

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const Envelope = require('./models/Envelope')
 const app = express()
 const PORT = 4055
 
+let envelopes = []
+
+app.use(express.json())
+
+
 app.get('/', (req, res) => {
     res.send('ok')
+})
+
+app.get('/envelopes', (req, res) => {
+    res.send(envelopes)
+})
+
+app.post('/envelopes', (req, res) => {
+    const envelope = new Envelope(req.body)
+    if (!envelope || Object.keys(envelope) == 0) {
+        res.status(400).send({ error: 'invalid envelope' })
+        return;
+    }
+    envelopes.push(envelope)
+    res.send(envelope)
 })
 
 app.listen(PORT, () => {
---
+++ models/Envelope.js
@@ -0,0 +1,31 @@
+class Envelope {
+    constructor(obj) {
+        if (!isValidEnvelope(obj)) return null;
+        this.name = obj.name;
+        this.limit = obj.limit
+    }
+}
+
+function isValidNum(param) {
+    return typeof (param) == 'number'
+}
+
+function isValidName(param) {
+    if (typeof (param) == 'string' &&
+        param.length > 3 &&
+        param.length < 20) {
+        return true;
+    }
+    return false;
+}
+
+function isValidEnvelope(obj) {
+    if (
+        isValidNum(obj.limit) &&
+        isValidName(obj.name)) {
+        return true;
+    }
+    return false;
+}
+
+module.exports = Envelope;
